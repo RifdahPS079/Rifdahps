75dcee75c1034729c4b5b01b0199021455270897
nurululmi7 <nurululmii04@gmail.com>
2024-06-26 16:06:46 +0700

    membuat index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!-- Nurul Ulmi Mustafa= Membuat index.php-->
  
 <?php
+session_start();
+
 // Koneksi ke database
 $servername = "localhost"; 
 $username = "root"; 
@@ -15,13 +17,9 @@ if (!$conn) {
 }
 
 // Ambil data dari database dan tampilkan
-
-
-
 if (isset($_GET['cari'])) {
     $resep = $_GET['cari'];
     $sql = "SELECT * FROM unggah_resep WHERE nama_resep LIKE '%$resep%'";
-
 } else {
     $sql = "SELECT * FROM unggah_resep";
 }
@@ -39,65 +37,64 @@ $result = mysqli_query($conn, $sql);
 </head>
 <body>
 <header>
-        <div class="container"> 
-            <nav>
-                <ul class="nav-log">
-                    <li><a href="daftar.php">Daftar</a></li>
-                    <li><a href="login.php">Login</a></li>        
-                    
-                </ul>
-            </nav>
-            <h1 class="logo">FoodRecipes</h1>
-            <nav>
-                <ul class="nav-list">
-                    <li><a href="index.php">Home</a></li>              
-                    <li><a href="tentang.php">Tentang</a></li>
-                    <li><a href="unggah_resep.php">Unggah Resep</a></li>
-                    <?php if (isset($_SESSION['user_id'])) {
-                        echo "<li><a href='resep_disimpan.php?user_id=".$_SESSION['user_id']."'>Unggah Resep</a></li>";
-                    } ?>
-                    <li><a href="profil.php">Profil</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+    <div class="container"> 
+        <nav>
+            <ul class="nav-log">
+                <li><a href="daftar.php">Daftar</a></li>
+                <li><a href="login.php">Login</a></li>        
+            </ul>
+        </nav>
+        <h1 class="logo">FoodRecipes</h1>
+        <nav>
+            <ul class="nav-list">
+                <li><a href="index.php">Home</a></li>              
+                <li><a href="tentang.php">Tentang</a></li>
+                <li><a href="unggah_resep.php">Unggah Resep</a></li>
+                <?php if (isset($_SESSION['user_id'])) {
+                    echo "<li><a href='resep_disimpan.php?user_id=".$_SESSION['user_id']."'>Unggah Resep</a></li>";
+                } ?>
+                <li><a href="profil.php">Profil</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
 
-    <section class="hero">
-        <div class="hero-section">
-            <h2>Selamat Datang Di Dunia Resep</h2>
-            <p>Temukan Resep Yang Kamu Inginkan</p>
+<section class="hero">
+    <div class="hero-section">
+        <h2>Selamat Datang Di Dunia Resep</h2>
+        <p>Temukan Resep Yang Kamu Inginkan</p>
 
-            <form action="#" class="search-box" >
-                <input type="text" placeholder="Cari Resep" name="cari">
-                <button type="submit">Cari</button>
-            </form>
-        </div>
-    </section>
+        <form action="#" class="search-box">
+            <input type="text" placeholder="Cari Resep" name="cari">
+            <button type="submit">Cari</button>
+        </form>
+    </div>
+</section>
 
-    <section class="recipes">
-        <h1>KUMPULAN RESEP</h1>
-        <div class="recipe-section">
-            <?php
-            if (mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo '<div class="recipe-card">';
-                    echo '<img src="uploads/' . $row['gambar'] . '" alt="' . $row['nama_resep'] . '">';
-                    echo '<h2>' . $row['nama_resep'] . '</h2>';
-                    echo '<a href="detail_resep.php?id=' . $row['id'] . '">Lihat Resep</a>';
-                    echo '</div>';
-                }
-            } else {
-                echo "Tidak ada resep yang ditemukan";
+<section class="recipes">
+    <h1>KUMPULAN RESEP</h1>
+    <div class="recipe-section">
+        <?php
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo '<div class="recipe-card">';
+                echo '<img src="uploads/' . $row['gambar'] . '" alt="' . $row['nama_resep'] . '">';
+                echo '<h2>' . $row['nama_resep'] . '</h2>';
+                echo '<a href="detail_resep.php?id=' . $row['id'] . '">Lihat Resep</a>';
+                echo '</div>';
             }
-            ?>
-        </div>
-    </section>
+        } else {
+            echo "Tidak ada resep yang ditemukan";
+        }
+        ?>
+    </div>
+</section>
 
-    <footer>
-        <div>
-            <p>&copy; 2024 Project WEB</p>
-        </div>  
-    </footer>
+<footer>
+    <div>
+        <p>&copy; 2024 Project WEB</p>
+    </div>  
+</footer>
 </body>
 </html>
 
